b7c7dfefd2f968691673edd68fa65b6cbc0eaf6e
Danielle Grogan <dgrogan@squam.sr.unh.edu>
2020-01-30 11:49:01 -0500

    short code to list days of month as id

---
+++ month_day_ids.R
@@ -0,0 +1,12 @@
+# month_day_ids()
+
+# make a list of ids (1 through 12), repeated by the number of days in each month
+# useful to converting daily to monthly values using apply functions (e.g., StackApply)
+
+# non-leap year
+month.data = read.csv("/net/home/eos/dgrogan/git_repos/WBMr/data/days_in_months.csv")
+m.ids = unlist(mapply(FUN = function(x,y)rep(x,y), x = seq(1,12), y = month.data$days))
+
+# leap year
+month.data.leap = read.csv("/net/home/eos/dgrogan/git_repos/WBMr/data/days_in_months_leap.csv")
+m.ids.leap = unlist(mapply(FUN = function(x,y)rep(x,y), x = seq(1,12), y = month.data.leap$days))

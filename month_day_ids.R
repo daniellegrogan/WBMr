54341dc418290564af18d03cff809c3a2bd81a83
Danielle Grogan <dgrogan@squam.sr.unh.edu>
2020-01-30 11:50:35 -0500

    made into function

--- month_day_ids.R
+++ month_day_ids.R
@@ -3,10 +3,17 @@
 # make a list of ids (1 through 12), repeated by the number of days in each month
 # useful to converting daily to monthly values using apply functions (e.g., StackApply)
 
-# non-leap year
-month.data = read.csv("/net/home/eos/dgrogan/git_repos/WBMr/data/days_in_months.csv")
-m.ids = unlist(mapply(FUN = function(x,y)rep(x,y), x = seq(1,12), y = month.data$days))
+month_day_ids = function(leap  # T or F.  T for leap year, F for non-leap year
+                         ){
+  if(leap == T){
+    # leap year
+    month.data.leap = read.csv("/net/home/eos/dgrogan/git_repos/WBMr/data/days_in_months_leap.csv")
+    m.ids = unlist(mapply(FUN = function(x,y)rep(x,y), x = seq(1,12), y = month.data.leap$days))
+  }else{
+    # non-leap year
+    month.data = read.csv("/net/home/eos/dgrogan/git_repos/WBMr/data/days_in_months.csv")
+    m.ids = unlist(mapply(FUN = function(x,y)rep(x,y), x = seq(1,12), y = month.data$days))
+  }
+  m.ids
+}
 
-# leap year
-month.data.leap = read.csv("/net/home/eos/dgrogan/git_repos/WBMr/data/days_in_months_leap.csv")
-m.ids.leap = unlist(mapply(FUN = function(x,y)rep(x,y), x = seq(1,12), y = month.data.leap$days))

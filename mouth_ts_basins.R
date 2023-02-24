8666fafb409d8b4c14b76589b390dfba345c6002
Danielle Grogan <dgrogan@yukon.sr.unh.edu>
2023-02-24 13:41:15 -0500

    added monthly time series option to mouth_ts_basins

--- mouth_ts_basins.R
+++ mouth_ts_basins.R
@@ -7,6 +7,7 @@ library(raster)
 rasterOptions(tmpdir = "/net/usr/spool/")   # set alternative /tmp directory
 library(rgdal)
 library(rgeos)
+library(lubridate)
 
 # wbm_load()
 wbm_load.script = getURL("https://raw.githubusercontent.com/daniellegrogan/WBMr/master/wbm_load.R", ssl.verifypeer=F)
@@ -16,13 +17,14 @@ eval(parse(text=wbm_load.script))
 mouth_ts.script = getURL("https://raw.githubusercontent.com/daniellegrogan/WBMr/master/mouth_ts.R", ssl.verifypeer=F)
 eval(parse(text=mouth_ts.script))
 
-mouth_ts_basins = function(basin.ID.list,     # list of basin ID values. Typically numeric, but could be character string. Must match basin_ID_grid file
-                           basin.ID.grid,     # grid acsii file giving the ID value for each grid cell
-                           up.area,           # upstream area file (ascii grid)
-                           path,              # character string: path to wbm output (directory with .nc files in it)
-                           varname,           # character string: variable to extract from basin mouth points
-                           yrs,               # years of wbm output to extract
-                           out.nm = NA        # optional name to save output
+### Yearly values
+mouth_ts_basins_annual = function(basin.ID.list,     # list of basin ID values. Typically numeric, but could be character string. Must match basin_ID_grid file
+                                  basin.ID.grid,     # grid acsii file giving the ID value for each grid cell
+                                  up.area,           # upstream area file (ascii grid)
+                                  path,              # character string: path to wbm output (directory with .nc files in it)
+                                  varname,           # character string: variable to extract from basin mouth points
+                                  yrs,               # years of wbm output to extract
+                                  out.nm = NA        # optional name to save output
 ){
   out.table = data.frame(matrix(nr=length(basin.ID.list), nc=length(yrs)))
   rownames(out.table) = basin.ID.list
@@ -34,7 +36,36 @@ mouth_ts_basins = function(basin.ID.list,     # list of basin ID values. Typical
                               path,               # path to wbm output
                               varname,            # variable name in wbm output file to extract
                               yrs)                # years of wbm output to extract
+    
+  }
+  if(!is.na(out.nm)){
+    write.csv(out.table, out.nm)
+    print(paste(out.nm, "written to file"))
+  }
+  return(out.table)
+}
 
+
+### Monthly values
+mouth_ts_basins_annual = function(basin.ID.list,     # list of basin ID values. Typically numeric, but could be character string. Must match basin_ID_grid file
+                                  basin.ID.grid,     # grid acsii file giving the ID value for each grid cell
+                                  up.area,           # upstream area file (ascii grid)
+                                  path,              # character string: path to wbm output (directory with .nc files in it)
+                                  varname,           # character string: variable to extract from basin mouth points
+                                  yrs,               # years of wbm output to extract
+                                  out.nm = NA        # optional name to save output
+){
+  out.table = data.frame(matrix(nr=length(basin.ID.list), nc=length(yrs)*12))
+  rownames(out.table) = basin.ID.list
+  colnames(out.table) = seq(from = ymd(paste(yrs[1], "01", "01", sep="-")), to=ymd(paste(yrs[length(yrs)], "12", "01", sep="-")), by="month")
+  for(i in 1:length(basin.ID.list)){
+    out.table[i, ] = mouth_ts(basin.ID.list[i],   # ID of the basin for which you want data from the mouth
+                              basin.ID.grid,      # basin ID file (ascii grid of basin IDs)
+                              up.area,            # upstream area file (ascii grid)
+                              path,               # path to wbm output
+                              varname,            # variable name in wbm output file to extract
+                              yrs)                # years of wbm output to extract
+    
   }
   if(!is.na(out.nm)){
     write.csv(out.table, out.nm)

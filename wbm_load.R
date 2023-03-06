05b693c93ceb5c3946315e5182ac35a99d162df4
Danielle Grogan <dgrogan@yukon.sr.unh.edu>
2023-03-06 11:09:25 -0500

    updated to use most recent version of sapply

--- wbm_load.R
+++ wbm_load.R
@@ -18,7 +18,7 @@ wbm_load = function(path, varname, years = NA){
       file.list = list.files(path = path, full.names = T)
     }else{
       file.list.full = list.files(path = path, full.names = T)
-      file.list = file.list.full[sapply(years, FUN = function(x) grep(pattern=paste("wbm_",x, sep=""), file.list.full))]
+      file.list = file.list.full[unlist(sapply(years, FUN = function(x) grep(pattern=paste("wbm_",x, sep=""), file.list.full)))]
     }
 
   wbm.brk = do.call(stack,
